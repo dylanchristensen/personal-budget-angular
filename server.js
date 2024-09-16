edc78db53e7433e1d554d2b42c9a9198dd23ab81
Dylan Christensen <helloamdylan@gmail.com>
2024-09-15 22:45:20 -0400

    dded server, Axios, and ChartJS :wq

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
+const Chart = require('chart.js');
 const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.use('/', express.static('public'));
+
+const budget = [
+    {
+        title: 'Food',
+        budget: 30
+    },
+    {
+        title: 'Rent',
+        budget: 350
+    },
+    {
+        title: 'Groceries',
+        budget: 90
+    },   
+];
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!!');
 });
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
