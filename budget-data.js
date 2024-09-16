1987c4a33d56028d5d9cc1cf63cf249d52f1b0f5
Dylan Christensen <helloamdylan@gmail.com>
2024-09-15 22:57:00 -0400

    Added server, Axios, and ChartJS

---
+++ budget-data.js
@@ -0,0 +1,32 @@
+{
+    "budget": [
+        {
+            "title": "Food",
+            "budget": 30
+        },
+        {
+            "title": "Rent",
+            "budget": 350
+        },
+        {
+            "title": "Groceries",
+            "budget": 90
+        },
+        {
+            "title": "Entertainment",
+            "budget": 50
+        },
+        {
+            "title": "Gas",
+            "budget": 70
+        },
+        {
+            "title": "Utilities",
+            "budget": 100
+        },
+        {
+            "title": "Insurance",
+            "budget": 80
+        }
+    ]
+}
--- server.js
+++ server.js
@@ -1,36 +1,22 @@
-const Chart = require('chart.js');
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const port = 3000;
-
 
 app.use('/', express.static('public'));
 
-const budget = [
-    {
-        title: 'Food',
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 350
-    },
-    {
-        title: 'Groceries',
-        budget: 90
-    },   
-];
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!!');
-});
-
+// Read budget data from JSON file
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    fs.readFile('budget-data.json', 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading budget data');
+            return;
+        }
+        const budget = JSON.parse(data).budget;
+        res.json(budget);
+    });
 });
-
-
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
